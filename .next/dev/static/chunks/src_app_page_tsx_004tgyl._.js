4677a35e1fd5dbda682dab2d145beae3417967b4
Noobmaster <nilesh0712006@gmail.com>
2026-06-08 23:09:09 +0530

    removed .next

--- .next/dev/static/chunks/src_app_page_tsx_004tgyl._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_@firebase_auth_dist_esm_1t9q1ao._.js","static/chunks/node_modules_@firebase_firestore_dist_1rzwrsl._.js","static/chunks/node_modules_066-ri1._.js","static/chunks/src_04eazp-._.js"],
-    source: "dynamic"
-});
