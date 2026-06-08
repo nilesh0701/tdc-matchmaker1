ea939cfd20aa883b69f0a8ae31a35fb74e947535
Noobmaster <nilesh0712006@gmail.com>
2026-06-08 22:19:43 +0530

    Added more UI adjustments got rid of the lanky bloatware, added landing page

--- .next/dev/static/chunks/src_app_page_tsx_004tgyl._.js
+++ .next/dev/static/chunks/src_app_page_tsx_004tgyl._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_1qac6xq._.js"],
+    chunks: ["static/chunks/node_modules_@firebase_auth_dist_esm_1t9q1ao._.js","static/chunks/node_modules_@firebase_firestore_dist_1rzwrsl._.js","static/chunks/node_modules_066-ri1._.js","static/chunks/src_04eazp-._.js"],
     source: "dynamic"
 });
